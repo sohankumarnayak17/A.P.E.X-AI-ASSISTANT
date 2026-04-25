6c731b32244adf5acd9d1649ce7edf10541a608d
sohankumarnayak17 <sohankumarnayak691@gmail.com>
2026-04-25 23:12:22 +0530

    auto-commit

--- engine/command.py
+++ engine/command.py
@@ -1,13 +1,32 @@
 import pyttsx3
 import speech_recognition as sr
+import sounddevice
+
 def speak(text):
     engine = pyttsx3.init('sapi5')
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[0].id)
     engine.setProperty('rate', 174)
-    print(voices)
     engine.say(text)
     engine.runAndWait()
 
-speak("teri maa ka bhosdaaa aaaaaaaaaaah ")
+def takecommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening...')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source, 10, 6)
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language="en-in")
+        print(f"You said: {query}")
+        speak(f"You said: {query}")
+    except Exception as e:
+        print("Could not understand. Try again.")
+        speak("Could not understand. Try again.")
+        return ""
+    return query.lower()
 
+text = takecommand()
+print(f"Final query: {text}")
