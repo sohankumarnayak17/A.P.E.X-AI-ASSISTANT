5d58df44880537d0d000fac4c74ed242d526be43
sohankumarnayak17 <sohankumarnayak691@gmail.com>
2026-04-28 12:51:26 +0530

    auto-commit

--- engine/feature.py
+++ engine/feature.py
@@ -1,11 +1,21 @@
 from playsound import playsound
 import eel
 import os
+import pywhatkit as kit
 from engine.config import ASSISTANT_NAME
 
 def playAssistantSound():
     music_dir = "front\\assets\\audio\\radio.mp3"
     playsound(music_dir)
+
+def speak(text):
+    import pyttsx3
+    engine = pyttsx3.init('sapi5')
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id)
+    engine.setProperty('rate', 174)
+    engine.say(text)
+    engine.runAndWait()
 
 def opencommand(query):
     query = query.replace(ASSISTANT_NAME, "")
@@ -20,11 +30,7 @@ def opencommand(query):
     else:
         speak('Please tell me what to open, Boss.')
 
-def speak(text):
-    import pyttsx3
-    engine = pyttsx3.init('sapi5')
-    voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[0].id)
-    engine.setProperty('rate', 174)
-    engine.say(text)
-    engine.runAndWait()
+def playyoutube(query):
+    search_term = query.replace("play", "").replace("on youtube", "").replace("youtube", "").strip()
+    speak("Playing " + search_term + " on YouTube, Boss.")
+    kit.playonyt(search_term)
